d5345ef9960bcbc6ccb931d65f30d2f8ae38d7b6
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-03-14 23:44:55 +0100

    add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('eslint').Linter[]} */
+export default [
+  tseslint.config(eslint.configs.recommended, tseslint.configs.recommended),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_.*',
+          varsIgnorePattern: '^_.*',
+        },
+      ],
+    },
+  },
+]
