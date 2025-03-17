21ce75f1eb0ab82834954793c5f211c751eef2fc
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-03-17 20:13:56 +0100

    add code block thanks frimousse for this

--- www/components/examples/examples.tsx
+++ www/components/examples/examples.tsx
@@ -1,11 +1,28 @@
-'use client'
-
+import { CodeBlock } from '@/components/ui/code-block'
 import { BasicExample1 } from './basics'
 
 export function Examples() {
   return (
-    <div className='flex flex-col gap-2'>
+    <div className='flex flex-col gap-2 w-full max-w-3xl items-center'>
       <BasicExample1 />
+      <CodeBlock lang='bash'>npm i konpo</CodeBlock>
+      <CodeBlock lang='tsx'>{`
+        import { Composer } from 'konpo'
+
+        export function CommentComposer() {
+          return (
+            <Composer.Root>
+              <Composer.Editor />
+              <Composer.FloatingToolbar>
+                <Composer.ToggleMarkTrigger>
+                  <BoldIcon />
+                </Composer.ToggleMarkTrigger>
+              </Composer.FloatingToolbar>
+              <Composer.SubmitTrigger />
+            </Composer.Root>
+          )
+        }
+      `}</CodeBlock>
     </div>
   )
 }
---
+++ www/hooks/use-mounted.ts
@@ -0,0 +1,12 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+export function useMounted(): boolean {
+  const [mounted, setMounted] = useState<boolean>(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return mounted
+}
